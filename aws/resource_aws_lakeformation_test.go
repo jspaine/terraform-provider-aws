87ab37a163aed26b215bf0fbcf50515779d3e77e
Daniel Messias <danielcmessias@gmail.com>
2021-06-03 15:54:14 +0100

    Validation and add tests to serial

--- aws/resource_aws_lakeformation_test.go
+++ aws/resource_aws_lakeformation_test.go
@@ -38,6 +38,11 @@ func TestAccAWSLakeFormation_serial(t *testing.T) {
 			"tableDataSource":            testAccAWSLakeFormationPermissionsDataSource_table,
 			"tableWithColumnsDataSource": testAccAWSLakeFormationPermissionsDataSource_tableWithColumns,
 		},
+		"PolicyTag": {
+			"basic":      testAccAWSLakeFormationPolicyTag_basic,
+			"disappears": testAccAWSLakeFormationPolicyTag_disappears,
+			"values":     testAccAWSLakeFormationPolicyTag_values,
+		},
 	}
 
 	for group, m := range testCases {
